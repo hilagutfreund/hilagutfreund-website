99b061bdc7b05403b51554765f17e97affb7f75a
hilagutfreund <hila.gut@gmail.com>
2016-06-18 01:40:14 +0300

    Got a basic version of instagram feed to appear on websitegit status

--- js/main.js
+++ js/main.js
@@ -1,36 +1,32 @@
-    // var feed = new Instafeed({
-    //     get: 'tagged',
-    //     tagName: 'awesome',
-    //     //clientId: '3271830693.467ede5.3d940be0e681483394ef1bc044270f05',
-    //     userId: "3271830693", 
-    //     accessToken: "3271830693.467ede5.3d940be0e681483394ef1bc044270f05",
-    //     template: '<a href="{{link}}"><img src="{{image}}" /></a>'
-    // });
-    // feed.run();
+// $(document).ready(function() {
+//     var accessToken = '3271830693.e8750bb.cb4e791f241b474d891c043abdac34c0';
+//     var userID = 3271830693;
+//     $(".instagram.search").instagram({
+//         userID = userID,
+//         show: 5,
+//         accessToken: accessToken
+//     });
+// });
 
 
-    // var feed = new Instafeed({
-    // userId: "3271830693", 
-    // accessToken: "3271830693.467ede5.3d940be0e681483394ef1bc044270f05",  
+  // $(document).ready(function(){
+  //   jQuery(function($) {
+  //         $('.instagram').on('willLoadInstagram', function(event, options) {
+  //           console.log(options);
+  //         });
+  //         $('.instagram').on('didLoadInstagram', function(event, response) {
+  //           console.log(response);
+  //         });
+  //         $('.instagram').instagram({
+  //         userId: 3271830693,
+  //         accessToken: '3271830693.e8750bb.cb4e791f241b474d891c043abdac34c0'
+  //       });
 
-$(document).ready(function(){
- var userfeed = new Instafeed({
-        get: 'user',
-        userId: 3271830693, 
-        accessToken: "3271830693.467ede5.3d940be0e681483394ef1bc044270f05", 
-        filter: function(image){
-            return image.user.username === "hilariousdesigns"; 
-        },
-        useHttp: true,
-        sortBy: "most-recent",
-        limit: 20
-    });
-    userfeed.run();
-}); 
+  //       })
+  //   }); 
 
 
 var scotchApp = angular.module('scotchApp', ['ngRoute', 'ui.router']);
-
     // configure our routes
     // scotchApp.config(function($routeProvider) {
     //     $routeProvider
@@ -68,15 +64,46 @@ var scotchApp = angular.module('scotchApp', ['ngRoute', 'ui.router']);
             templateUrl: 'templates/projects.html'
         })
     });
-
+   
     // create the controller and inject Angular's $scope
     scotchApp.controller('mainController', function($scope) {
         // create a message to display in our view
         $scope.message = 'Everyone come and see how good I look!';
     });
-
-    scotchApp.controller('portfolioController', function($scope) {
+    
+    scotchApp.controller('portfolioController', function($scope, $rootScope) {
         $scope.message = 'Look! I am an about page.';
+        $scope.imagesReady = false; 
+        $scope.$on('photosready', function(events, args){
+            $scope.photosready = true; 
+            $scope.myfeed= args; 
+            console.log($scope.photosready);
+            console.log($scope.myfeed);
+            $scope.$root.$digest()
+
+        })
+
+        jQuery(function($) {
+          $('.instagram').on('willLoadInstagram', function(event, options) {
+            console.log(options);
+          });
+          $('.instagram').on('didLoadInstagram', function(event, response) {
+            //console.log(response);
+            $scope.images = response['data']; 
+            //$scope.imagesReady = true; 
+            $rootScope.$broadcast('photosready', $scope.images);
+            //console.log($scope.images[0]['images']['thumbnail']);
+          });
+         $('.instagram').instagram({
+                  userId: 3271830693,
+                  accessToken: '3271830693.e8750bb.cb4e791f241b474d891c043abdac34c0'
+                });
+
+            });
+       
+
+
+        
     });
 
     scotchApp.controller('projectsController', function($scope) {
