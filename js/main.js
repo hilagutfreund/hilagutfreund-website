69bda4ff0c2592948e6331e35c00329064256b86
hilagutfreund <hila.gut@gmail.com>
2016-06-23 13:16:59 +0300

    added documents and resume

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var hilaApp = angular.module('hilaApp', ['ngRoute', 'ui.router']);
 
     hilaApp.config(function($stateProvider, $urlRouterProvider){
         $urlRouterProvider.otherwise('portfolio');
+        // $urlRouterProvider.when('resume', 'documents-forweb/HilaGutfreund-Resume-2016.pdf');
         $stateProvider
         .state('portfolio', {
             url: '/portfolio',
@@ -9,9 +10,11 @@ var hilaApp = angular.module('hilaApp', ['ngRoute', 'ui.router']);
             controller: 'portfolioController'
         })
 
-        .state('resume', {
-            url: '/resume', 
-            templateUrl: 'templates/projects.html'
+        .state('about', {
+            url: '/about', 
+            templateUrl: "templates/about.html", 
+            controller: 'aboutController'
+
         })
     });
    
@@ -19,6 +22,7 @@ var hilaApp = angular.module('hilaApp', ['ngRoute', 'ui.router']);
     hilaApp.controller('mainController', function($scope) {
         // create a message to display in our view
         $scope.message = 'Everyone come and see how good I look!';
+        $scope.resume = "documents-forweb/HilaGutfreund-Resume-2016.pdf"; 
     });
     
     hilaApp.controller('portfolioController', function($scope, $rootScope) {
@@ -78,19 +82,49 @@ var hilaApp = angular.module('hilaApp', ['ngRoute', 'ui.router']);
 
          $scope.mydocuments = [
 
-        {
-                "title": "WDJC", 
-                "description": "DJ is a procedural scripting language for algorithmic music production.", 
-                "year": "PLT, 2013",
-                "link": "http://whet-plt.github.io/wdjc/", 
-                "image": "img/WHET.png",
-                "other": null
+            {
+                "title": "Home", 
+                "break": null,
+                "description": "A project on the concept of home through journal entries and works of Modern Jewish Literature.", 
+                "year": "JTS Senior Seminar, 2013",
+                "link": "documents-forweb/homethroughjournals.pdf", 
+                "image": "img/home.jpg",
+                "other": "documents-forweb/finalexplanation-seniorseminar.pdf", 
+                "otherDesc": "Explanation of project"
             }, 
+            {
+                "title": "Butler Library", 
+                "break": null,
+                "description": "An essay exploring the Columbia University Library, Butler, as a type of home.", 
+                "year": "The Art of Being Onself, 2011",
+                "link": "documents-forweb/butlerlibrary.pdf", 
+                "image": "img/butler.jpg",
+                "other": null, 
+                "otherDesc": null
+            }, 
+            {
+                "title": "Brou-Ha-What?",
+                "break": 'br',
+                "description": "An essay detailing how the word 'brouhaha' has become an integral part of my life.", 
+                "year": "The Art of Being Oneself, 2011",
+                "link": "documents-forweb/brouhaha-artofbeingonself.pdf", 
+                "image": "img/brouhaha.gif",
+                "other": null,
+                "otherDesc": null
+            }, 
+            {
+                "title": "Margalit Street", 
+                "break": 'br',
+                "description": "An essay detailing my connection to my old neighborhood and house.", 
+                "year": "The Art of the Essay, 2009",
+                "link": "documents-forweb/journeytochildhood.pdf", 
+                "image": "img/street.jpg",
+                "other": null, 
+                "otherDesc": null
+            } 
         ]
     });
 
-
-
-    hilaApp.controller('projectsController', function($scope) {
+    hilaApp.controller('aboutController', function($scope) {
         $scope.message = 'projects! JK. This is just a demo.';
     });
