f633c049d8cc902c9c23b6d90ec61d71ad1060e3
hilagutfreund <hila.gut@gmail.com>
2018-01-15 00:26:36 -0800

    added amazon music

--- js/main.js
+++ js/main.js
@@ -79,6 +79,14 @@ var hilaApp = angular.module('hilaApp', ['ngRoute', 'ui.router']);
                 "link": "https://hilagutfreund.github.io/BFQuiz/", 
                 "docLink": "https://hilagutfreund.github.io/BFQuiz/",
                 "image": "img/bf-badge.png" 
+            },
+            {
+                "title": "Heuristic Evaluation", 
+                "description": "A usability evaluation for Amazon Music app.", 
+                "year": "SJSU, 2017",
+                "link": "https://www.hilariousdesigns.com/EngineeringPsychology/AmazonMusicApp/", 
+                "docLink": "https://drive.google.com/file/d/1I2wJgH3MNeAWVzPltwA_Gi1PlcnsWG5Z/view?usp=sharing",
+                "image": "img/amazonmusic.png" 
             }
         ];
 
